98770f4e9e60dd7a7355b728f52104760e9a689d
Sangam Shrestha <2shrestha22@gmail.com>
2023-09-16 20:41:41 +0545

    remove dynamic theme

--- lib/src/app/app.dart
+++ lib/src/app/app.dart
@@ -1,4 +1,3 @@
-import 'package:dynamik_theme/dynamik_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:weather/src/app/dashboard_page.dart';
 import 'package:weather/src/core/constant/app_colors.dart';
@@ -8,36 +7,16 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DynamikTheme(
-      config: ThemeConfig(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.from(
         useMaterial3: true,
-        // You can also generate color schemes from:
-        // https://m3.material.io/theme-builder#/custom
-        lightScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primary,
-        ),
-        darkScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primary,
-          brightness: Brightness.dark,
-        ),
-        defaultThemeState: SimpleThemeType.dynamik.themeState,
-        builder: (themeData) {
-          // Add more customization on ThemeData.
-          return themeData.copyWith(
-            appBarTheme: const AppBarTheme(centerTitle: true),
-            inputDecorationTheme: const InputDecorationTheme(
-              border: OutlineInputBorder(),
-            ),
-          );
-        },
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
+      ).copyWith(
+        appBarTheme: const AppBarTheme(centerTitle: true),
       ),
-      builder: (theme, darkTheme, themeMode) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: theme,
-        darkTheme: darkTheme,
-        title: 'Weather',
-        home: const DashboardPage(),
-      ),
+      title: 'Weather',
+      home: const DashboardPage(),
     );
   }
 }
